cbbeef22672e197af08379d7983e813bee8813ba
haryoiro <55312590+haryoiro@users.noreply.github.com>
2019-10-26 04:15:36 +0900

    Fix -

--- js/radius.js
+++ js/radius.js
@@ -1,28 +1,19 @@
-
-function setRadius(newRadius){
-    if(newRadius<minRad)
-        newRadius = minRad;
-    else if(newRadius>maxRad)
-        newRadius = maxRad;
-    radius = newRadius;
-    ctx.lineWidth = radius * 2;
-
-    radSpan.innerHTML = radius;
-}
-
 let minRad = 1,
     maxRad = 20,
     defaultRad = 1,
     interval = 1,
     radSpan = document.getElementById('radval'),
-    decRad = document.getElementById('decrad'),
-    incRad = document.getElementById('incrad');
+    decRad = document.getElementById('decrad').addEventListener('click', function(){setRad(radius-interval);}),
+    incRad = document.getElementById('incrad').addEventListener('click', function(){setRad(radius+interval);});
 
+function setRad(newRad){
+    if(newRad<minRad)
+        newRad = minRad;
+    else if(newRad>maxRad)
+        newRad = maxRad;
+    radius = newRad;
+    ctx.lineWidth = radius * 2;
+    radSpan.innerHTML = radius;
+}
 
-decRad.addEventListener('click', function(){
-    setRadius(radius-interval);
-});
-incRad.addEventListener('click', function(){
-    setRadius(radius+interval);
-});
-setRadius(defaultRad);
+setRad(defaultRad);
