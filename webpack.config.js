102ad602b944e234f724faa2757695623370c98e
haryoiro <mizusecocolte@gmail.com>
2020-02-08 14:46:21 +0900

    update add server.js build config and add plugins for nodemonPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,67 @@
 const path = require('path')
+const nodeExtarnals = require("webpack-node-externals")
+const NodemonPlugin = require("nodemon-webpack-plugin")
 
-module.exports = {
+module.exports = [{
+  // // ---- ブラウザで動く機能のバンドル
   // モード選択
   // development -> ソースマップ有効
   // production  -> 最適化有効
   mode: "development",
 
   // エントリーポイントの設定（メインとなるJavaScriptファイル）
-  entry: "./src/main.ts",
-
+  entry: {
+    client: "./src/client/index.ts"
+  },
+  // 出力設定
+  output: {
+    filename: 'bundle.js',
+    // expressでpublicフォルダ直下を静的に読み込むように設定するので、そこへ出力
+    path: `${__dirname}/public/js`
+  },
   module: {
-    rules: [
-      {
+    rules: [{
         // 拡張子 .ts の場合
         test: /\.ts$/,
         use: "ts-loader",
+        exclude: /node_modules/,
       }
     ]
   },
   resolve: {
     extensions: [".js",".ts"]
   },
-  // 出力設定
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'build')
+}, {
+  // // ---- Nodeサーバーで動く機能をバンドル
+  // モード選択
+  // development -> ソースマップ有効
+  // production  -> 最適化有効
+  mode: "development",
+  
+  // エントリーポイントの設定（メインとなるJavaScriptファイル）
+  entry: {
+    server: "./src/server/index.ts"
+  },
+  target: "node",
+  node: {
+    // expressを使用する際はこの設定をしないとエラーとなる
+    // 参考：https://medium.com/@binyamin/creating-a-node-express-webpack-app-with-dev-and-prod-builds-a4962ce51334
+    __dirname: false,
+    __filename: false,
+  },
+
+  externals: [nodeExtarnals()],
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: "ts-loader",
+      exclude: "/node_modules/",
+    }]
+  },
+  plugins: [
+    new NodemonPlugin()
+  ],
+  resolve: {
+    extensions: [".js",".ts"]
   }
-}
+}]
