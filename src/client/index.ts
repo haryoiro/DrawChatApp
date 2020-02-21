b2d1963cb8ea72551b37b366c0811252f887d271
haryoiro <mizusecocolte@gmail.com>
2020-02-21 23:10:54 +0900

    fix better method names && Mouse & PointerMethod on addEventListener moved to Tools class

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,363 +1,385 @@
 class Application {
-  canvas: HTMLCanvasElement
-  ctx: any
-  width: number
-  height: number
-  fps: number
-  rect: ClientRect
-  constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, width: number, height: number) {
-    this.width = width
-    this.height = height
-    this.canvas = canvas
-    this.ctx = context
-    this.rect = this.canvas.getBoundingClientRect()
+  canvas: HTMLCanvasElement;
+  ctx: any;
+  width: number;
+  height: number;
+  fps: number;
+  rect: ClientRect;
+  constructor(
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ) {
+    this.width = width;
+    this.height = height;
+    this.canvas = canvas;
+    this.ctx = context;
+    this.rect = this.canvas.getBoundingClientRect();
   }
-  public init(context: CanvasRenderingContext2D, color: string, hide: boolean, smooth: boolean) {
-    this.canvasSize()
-    this.backgroundColor(color)
-    this.hideMenuHandler(hide)
-    this.isImageSmoothing(context, smooth)
+  public init(
+    context: CanvasRenderingContext2D,
+    color: string,
+    hide: boolean,
+    smooth: boolean = false
+  ) {
+    this.canvasSize();
+    this.backgroundColor(color);
+    this.hideMenuHandler(hide);
+    this._isImageSmoothing(context, smooth);
   }
   public getRect() {
     //@ts-ignore
-    return this.rect = this.canvas.getBoundingRect()
+    return (this.rect = this.canvas.getBoundingRect());
   }
   private canvasSize() {
     if (this.width || this.height) {
-      this.canvas.width = this.width
-      this.canvas.height = this.height
-      this.ctx.width = this.width
-      this.ctx.height = this.height
+      this.canvas.width = this.width;
+      this.canvas.height = this.height;
+      this.ctx.width = this.width;
+      this.ctx.height = this.height;
     }
   }
   public clearAll() {
-    this.ctx.clearRect(0, 0, this.width, this.height)
-    this.ctx.beginPath()
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.beginPath();
   }
   public clearInterval(fps: number) {
-    this.fps = fps
+    this.fps = fps;
   }
   private backgroundColor(color: string) {
-    this.ctx.fillStyle = color
-    this.ctx.fillRect(0, 0, this.width, this.height)
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(0, 0, this.width, this.height);
   }
   private hideMenuHandler(bool: boolean) {
-    if (bool) {
-      document.addEventListener('contextmenu', e => { e.preventDefault() })
-      document.addEventListener('MSHoldVisal', e => { e.preventDefault() })
-    }
+    document.addEventListener("contextmenu", () => {
+      bool;
+    });
+    document.addEventListener("MSHoldVisal", () => {
+      bool;
+    });
   }
-  private isImageSmoothing(context: CanvasRenderingContext2D, bool: boolean) {
-    context.imageSmoothingEnabled = bool ? bool : true
-  }
-}
-class DrawEvents {
-  element: HTMLCanvasElement
-  ctx: CanvasRenderingContext2D
-  x: number
-  y: number
-  constructor(element: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-    this.x
-    this.y
-    this.element = element
-    this.ctx = context
-  }
-
-  public thisPoints(e: MouseEvent, rect: ClientRect) {
-
-    this.x = e.clientX - Math.floor(rect.left)
-    this.y = e.clientY - Math.floor(rect.top)
-  }
-  public mousePoints() {
-    return { x: this.x, y: this.y }
-  }
-  public harvestPoints(e: MouseEvent, rect: ClientRect) {
-    this.thisPoints(e, rect)
-    return this.mousePoints()
+  private _isImageSmoothing(context: CanvasRenderingContext2D, bool: boolean) {
+    context.imageSmoothingEnabled = bool;
   }
 }
 
-let eventStack: PointerEvent[] = []
-/**
- *
- *
- * @class Tools
- * @classdesc ドローイングに関するイベントハンドラ用関数
- */
+let eventStack: PointerEvent[] = [];
+
 class Tools {
   // ----- DOM要素 -----
-  element: HTMLCanvasElement
-  context: CanvasRenderingContext2D
-  pointerEventHandler: boolean
+  element: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+  pointerEventHandler: boolean;
   // ----- ツール関連プロパティ
-  canvasColor: string
-  drawToggle: boolean
-  eraserToggle: boolean
-  pressureToggle: boolean
+  canvasColor: string | number = "#000";
+  drawToggle: boolean;
+  eraserToggle: boolean;
+  pressureToggle: boolean;
   // ----- Pinch/Zoom用プロパティ -----
-  pinchHandler: any
-  baseX: any
-  baseY: any
-  nowX: any
-  nowY: any
-  nowR: any
-  p1: PointerEvent
-  p2: PointerEvent
-  dist: number = void 0
+  pinchDist: number;
+  baseX: number;
+  baseY: number;
+  nowX: number;
+  nowY: number;
+  nowR: number;
+  p1: PointerEvent;
+  p2: PointerEvent;
+  dist: number;
   // ----- PenSize用プロパティ -----
-  defRad: number = 10
-  penRadius: number
-  dx: number = void 0
-  dy: number = void 0
-  distX: number = void 0
-  distY: number = void 0
-  lx: number = void 0
-  ly: number = void 0
+  defRad: number = 10;
+  penRadius: number;
+  dx: number = void 0;
+  dy: number = void 0;
+  distX: number = void 0;
+  distY: number = void 0;
+  lx: number = void 0;
+  ly: number = void 0;
+  capStyle: any;
 
-  /**
-   * Toolsクラス内で使用する要素を設定
-   * @param {HTMLCanvasElement} element
-   * @param {CanvasRenderingContext2D} [context]
-   * @memberof Tools
-   */
   constructor(element: HTMLCanvasElement, context?: CanvasRenderingContext2D) {
-    this.element = element
-    this.context = this.element.getContext('2d')
+    this.element = element;
+    this.context = context;
+    this.eventActivation();
   }
-
-
-  // ポインターイベント用
-  public pencilTool(e: PointerEvent) {
-    //Context2D初期化
-    //@ts-ignore
-    let a: any = this.context
-    //消しゴムトグル
-    this.eraserToggle  ? a.globalCompositeOperation = 'destination-out'
-                              : a.globalCompositeOperation = 'source-over'
-
-    a.lineWidth = this.activatePressure(e)
-
-
-    a.strokeStyle = this.canvasColor
-    a.fillStyle = this.canvasColor
-    a.lineCap = 'round'
-
-    a.lineTo(e.offsetX, e.offsetY)
-    a.stroke()
-    a.beginPath()
-    a.moveTo(e.offsetX, e.offsetY)
+  public eventActivation() {
+    if (this._supportPointerEvent) {
+      document.addEventListener(
+        "pointerdown",
+        event => this.downPointerHandler(event),
+        { passive: false }
+      );
+      document.addEventListener(
+        "pointerup",
+        event => this.upPointerHandler(event),
+        { passive: false }
+      );
+      document.addEventListener(
+        "pointermove",
+        event => this.movePointerHandler(event),
+        { passive: false }
+      );
+      document.addEventListener(
+        "pointerleave",
+        event => this.leavePointerHandler(event),
+        { passive: false }
+      );
+    } else {
+      document.addEventListener("pointerdown", event =>
+        this.downMouseHandler(event)
+      );
+      document.addEventListener("pointerup", event =>
+        this.upMouseHandler(event)
+      );
+      document.addEventListener("pointermove", event =>
+        this.moveMouseHandler(event)
+      );
+      document.addEventListener("pointerleave", event =>
+        this.leaveMouseHandler(event)
+      );
+    }
   }
-
-  // 現在の座標を取得
-  // イベントを配列に格納
-  public handledown(e: PointerEvent) {
-    e.preventDefault()
-    this.drawToggle = true
-    this.nowX
-    this.nowY
-    eventStack.push(e)
+  // ---- PointerEvents ----
+  public downPointerHandler(event: PointerEvent) {
+    event.preventDefault();
+    this.drawToggle = true;
+    eventStack.push(event);
 
     if (eventStack.length <= 1) {
-      this.baseX = eventStack[0].pageX
-      this.baseY = eventStack[0].pageY
+      this.baseX = eventStack[0].pageX;
+      this.baseY = eventStack[0].pageY;
     } else if (eventStack.length >= 2) {
-      this.p1 = eventStack[0]
-      this.p2 = eventStack[1]
-      this.pinchHandler = Math.abs(this.p1.pageX - this.p2.pageX) + Math.abs(this.p1.pageY - this.p2.pageY)
+      this.p1 = eventStack[0];
+      this.p2 = eventStack[1];
+      this.pinchDist =
+        Math.abs(this.p1.pageX - this.p2.pageX) +
+        Math.abs(this.p1.pageY - this.p2.pageY);
     }
   }
-  public handlemove(e: PointerEvent) {
-    //touch
-    console.log(e.pointerType)
-    switch (e.pointerType) {
-      case 'pen':
-        this.handlePenMove(e)
+  public movePointerHandler(event: PointerEvent) {
+		this.pointerSwitcher(
+			event,{
+			'pen': this.handlePenMove(event),
+			'touch': this.handleTouchMove(event),
+			'mouse': this.handleMouseMove(event)
+			}
+		)
+	}
+	public pointerSwitcher(event: PointerEvent, functionObject: {pen: void, touch: void, mouse: void}){
+    switch (event.pointerType) {
+      case "pen":
+				functionObject.pen
         break;
-      case 'touch':
-        this.handleTouchMove(e)
+      case "touch":
+				functionObject.touch
         break;
-      case 'mouse':
-        this.handleMouseMove(e)
+      case "mouse":
+				functionObject.mouse
         break;
     }
-  }
-  public handleup(e: PointerEvent) {
-    this.drawToggle = false
+	}
+  public upPointerHandler(event: PointerEvent) {
+    this.drawToggle = false;
     //@ts-ignore
-    this.context.beginPath()
-    this.removeEventStack(e)
+    this.context.beginPath();
+    this._removeEventStack(event);
   }
-  public handleleave(e: PointerEvent) {
-    this.drawToggle = false
+  public leavePointerHandler(event: PointerEvent) {
+    this.drawToggle = false;
+  }
+
+  // ---- MouseEvents ----
+  public downMouseHandler(event: MouseEvent) {
+    this.drawToggle = true;
+  }
+  public moveMouseHandler(event: MouseEvent) {
+    this.handleMouseMove(event);
+  }
+  public upMouseHandler(event: MouseEvent) {
+    this.drawToggle = false;
+    //@ts-ignore
+    this.context.beginPath();
+  }
+  public leaveMouseHandler(event: MouseEvent) {
+    this.drawToggle = false;
   }
 
   // ----- moveHandler分岐処理 -----
-  //  ペン
-  public handlePenMove(e: PointerEvent) {
-    e.preventDefault()
+  //  ペン用moveHandler
+  public handlePenMove(event: PointerEvent) {
+    event.preventDefault();
     if (this.drawToggle) {
-      this.pencilTool(e)
+      this.penPencilTool(event);
     }
   }
-  // マウス
-  public handleMouseMove(e: PointerEvent) {
+  // マウス用moveHandler
+  public handleMouseMove(event: PointerEvent | MouseEvent) {
     if (this.drawToggle) {
-      this.pencilTool(e)
+      this.mousePencilTool(event);
     }
   }
-  // タッチ
-  public handleTouchMove(e: PointerEvent) {
-    e.preventDefault()
+  // タッチ用moveHandler
+  public handleTouchMove(event: PointerEvent) {
+    event.preventDefault();
     for (let i = 0; i < eventStack.length; i++) {
-      if (eventStack[i].pointerId == e.pointerId) {
-        eventStack[i] = e
+      if (eventStack[i].pointerId == event.pointerId) {
+        eventStack[i] = event;
       }
     }
 
     if (eventStack.length > 3) {
-      eventStack.splice( 0, 3 )
+      eventStack.splice(0, 3);
     }
-
-    this.p1 = eventStack[0]
-    this.p2 = eventStack[1]
-
+    this.p1 = eventStack[0];
+    this.p2 = eventStack[1];
     if (this.drawToggle) {
-      this.pencilTool(eventStack[0])
+      this.touchPencilTool(eventStack[0]);
     }
 
-    if (eventStack.length <= 1) {
+    if (eventStack.length >= 2) {
+      this.drawToggle = false;
+      this.lx = this.lx;
+      this.ly = this.ly;
+      this.dx = (this.p1.pageX + this.p2.pageX) / 2;
+      this.dy = (this.p1.pageY + this.p2.pageY) / 2;
 
-    } else if (eventStack.length >= 2) {
-      this.drawToggle = false
+      this.distX = Math.abs(this.lx - this.dx);
+      this.distY = Math.abs(this.ly - this.dy);
 
-
-      this.lx = this.lx
-      this.ly = this.ly
-      this.dx = (this.p1.pageX + this.p2.pageX) / 2
-      this.dy = (this.p1.pageY + this.p2.pageY) / 2
-
-      this.distX = Math.abs(this.lx - this.dx)
-      this.distY = Math.abs(this.ly - this.dy)
-
-      this.dist = Math.abs(this.p1.pageX - this.p2.pageX) + Math.abs(this.p1.pageY - this.p2.pageY)
-      this.nowR = this.dist / this.pinchHandler
+      this.dist =
+        Math.abs(this.p1.pageX - this.p2.pageX) +
+        Math.abs(this.p1.pageY - this.p2.pageY);
+      this.nowR = this.dist / this.pinchDist;
     }
-    this.pinchHandle()
+    this._pinchHandle();
   }
 
-  private puressurePoints(e: PointerEvent) {
-    return { x: e.offsetX, y: e.offsetY, pressure: Math.sin(e.pressure) }
+  // ---- PencilTools ----
+  // マウス用PencilTool
+  public mousePencilTool(event: MouseEvent | PointerEvent) {
+    //Context2D初期化
+    //@ts-ignore
+    let a: any = this.context;
+    //消しゴムトグル
+    this.eraserToggle
+      ? (a.globalCompositeOperation = "destination-out")
+      : (a.globalCompositeOperation = "source-over");
+
+    a.strokeStyle = this.canvasColor;
+    a.fillStyle = this.canvasColor;
+    a.lineCap = "round";
+    this.drawLine(a, event);
+  }
+  // ペン用PencilTool
+  public penPencilTool(event: PointerEvent) {
+    //Context2D初期化
+    //@ts-ignore
+    let a: any = this.context;
+    //消しゴムトグル
+    this.eraseTool(a);
+    a.lineWidth = this._activatePressure(event);
+    this.settingPenConf(a);
+    this.drawLine(a, event);
   }
 
-  private removeEventStack(e: PointerEvent) {
+  private settingPenConf(a: any) {
+    a.strokeStyle = this.canvasColor;
+    a.fillStyle = this.canvasColor;
+    a.lineCap = this.capStyle;
+  }
+
+  // タッチ用PencilTool
+  public touchPencilTool(event: PointerEvent) {
+    //Context2D初期化
+    //@ts-ignore
+    let a: any = this.context;
+    //消しゴムトグル
+    this.eraseTool(a);
+    a.lineWidth = this.penRadius;
+    this.settingPenConf(a);
+
+    this.drawLine(a, event);
+  }
+
+  private drawLine(a: any, event: PointerEvent | MouseEvent) {
+    a.lineTo(event.offsetX, event.offsetY);
+    a.stroke();
+    a.beginPath();
+    a.moveTo(event.offsetX, event.offsetY);
+  }
+
+  private eraseTool(a: any) {
+    this.eraserToggle
+      ? (a.globalCompositeOperation = "destination-out")
+      : (a.globalCompositeOperation = "source-over");
+  }
+
+  public setPencilColor(color: string | number) {
+    this.canvasColor = color;
+  }
+  public getNowR() {
+    return this.nowR;
+  }
+
+  // 筆圧に対応していた場合値を返す
+  private _activatePressure(event: PointerEvent) {
+    let Rad = this.defRad;
+    if (event.pressure < 0.995 || event.pressure > 0.05) {
+      event.pressure ? (Rad *= event.pressure) : (Rad /= event.pressure);
+      return Rad;
+    } else if (event.pressure <= 0.05 || event.pressure > 0.01) {
+      return 0.05;
+    } else {
+      return void 0;
+    }
+  }
+  //タッチイベントをスタックから削除
+  private _removeEventStack(event: PointerEvent) {
     for (let i = 0; i < eventStack.length; i++) {
-      if (eventStack[i].pointerId == e.pointerId) {
-        eventStack.splice( i, 1 )
+      if (eventStack[i].pointerId == event.pointerId) {
+        eventStack.splice(i, 1);
       }
     }
   }
-
-  // マウスイベント
-  public mousePencilTool(e: MouseEvent) {
-    //Context2D初期化
-    //@ts-ignore
-    let a: any = this.context
-    //消しゴムトグル
-    this.eraserToggle ? a.globalCompositeOperation = 'destination-out'
-      : a.globalCompositeOperation = 'source-over'
-
-    // 現在のポインター座標を取得
-    // {x: number, y: number}
-    let obj: { [s: string]: number } = DrawEvent.harvestPoints(e, app.rect)
-
-    a.strokeStyle = this.canvasColor
-    a.fillStyle = this.canvasColor
-    a.lineCap = 'round'
-
-    a.lineTo(e.offsetX, e.offsetY)
-    a.stroke()
-    a.beginPath()
-    a.moveTo(e.offsetX, e.offsetY)
+  // ピンチズーム処理
+  private _pinchHandle() {
+    let style = document.getElementById("canvas").style;
+    let scale = `scale(${this.nowR},${this.nowR})`;
+    style.left = this.distX + "px";
+    style.top = this.distY + "px";
+    style.transform = scale;
+    style.webkitTransform = scale;
   }
-  public handlemousedown(e: MouseEvent) {
-    this.drawToggle = true
+  private _supportPointerEvent() {
+    return window.PointerEvent ? true : false;
   }
-  public handlemouseup(e: MouseEvent) {
-    this.drawToggle = false
-    //@ts-ignore
-    let a: any = this.context
-    a.beginPath()
+  private _puressurePoints(event: PointerEvent) {
+    return {
+      x: event.offsetX,
+      y: event.offsetY,
+      pressurevent: Math.sin(event.pressure)
+    };
   }
-  public handlemousemove(e: MouseEvent) {
-    if (this.drawToggle) {
-      this.mousePencilTool(e)
+
+  // ----- 画面上にデバッグ情報が流れる
+  private _debugLogger(message: string | number) {
+    if (document.getElementById("debug")) {
+      document
+        .getElementById("debug")
+        .insertAdjacentHTML("afterbegin", message + "<br>");
+    } else {
+      let el = document.createElement("div");
+      el.id = "debug";
+      el.insertAdjacentHTML("afterbegin", message + "<br>");
     }
   }
-  public setPencilColor(color: string) {
-    this.canvasColor = color
-  }
-  public pinchHandle() {
-    let style = document.getElementById('canvas').style
-    let scale = `scale(${this.nowR},${this.nowR})`
-    style.left = this.distX + 'px'
-    style.top = this.distY + 'px'
-    style.transform = scale
-    style.webkitTransform = scale
-    this.p1 = void 0
-    this.p2 = void 0
-    this.dist = 0
-  }
-  public getNowR() {
-    return this.nowR
-  }
-  private activatePressure(e: PointerEvent) {
-    let Rad = this.defRad
-    if (e.pressure) {
-      console.log(e.pressure)
-      e.pressure ? Rad *= Math.sin(e.pressure)
-        : Rad /= Math.sin(e.pressure)
-      return Rad
-    }
-    else { return Rad }
-  }
 }
+const graph: HTMLCanvasElement = document.querySelector("#canvas");
+const c: CanvasRenderingContext2D = graph.getContext("2d");
 
-const graph: HTMLCanvasElement = document.querySelector('#canvas')
-const c: CanvasRenderingContext2D = graph.getContext('2d')
+const app = new Application(graph, c, 1920, 1080);
+const draw = new Tools(graph, c);
 
-const app = new Application(graph, c, 1080, 720)
-// app.prototype.init (context , backgroundColor, hidemenu, smoothRendering)
-app.init(c, "#ffff", true, false)
-const DrawEvent = new DrawEvents(graph, c)
-const draw = new Tools(graph, c)
-c.beginPath()
-
-if (window.PointerEvent) {
-  document.addEventListener('pointerdown', e => draw.handledown(e), { passive: false })
-  document.addEventListener('pointerup', e => draw.handleup(e), { passive: false })
-  document.addEventListener('pointermove', e => draw.handlemove(e), { passive: false })
-  document.addEventListener('pointerleave', e => draw.handleleave(e), { passive: false })
-} else {
-  graph.addEventListener('mousedown', e => draw.handlemousedown(e))
-  graph.addEventListener('mousemove', e => draw.handlemouseup(e))
-  graph.addEventListener('mouseup', e => draw.handlemousemove(e))
-  graph.addEventListener('mouseover', e => draw.handlemouseup(e))
-}
-
-// ----- 画面上にデバッグ情報が流れる
-function debug(str: string) {
-  document.getElementById('debug').insertAdjacentHTML('afterbegin', str + '<br>')
-}
-
-// ----- 要らなくなったかも -----
-// 挙動がおかしくなったときeventStack内を初期化
-// 連続で指が三本触れた時、removeEventが追いつかなくて前回格納されたイベントがそのまま配列内に残ってしまっ
-// eventStackのレングスが3以上になった場合  先の3を削除する事で対応
-document.getElementById('debunB').addEventListener('click', (): void => {
-  eventStack.splice(0, 10)
-  c.beginPath()
-})
-
-document.getElementById('clear').addEventListener('click', (): void => {
-  app.clearAll()
-})
+// Application.prototype.init
+//    (context , backgroundColor, hideMenu, smoothRendering)
+app.init(c, "#ffff", true, false);
