dc1c57e78165d6fb5ea92824e8c7205e2fd007ae
haryoiro <55312590+haryoiro@users.noreply.github.com>
2020-03-14 08:09:32 +0900

    comment out

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http"
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
+//import bodyParser from "body-parser";
+//import cookieParser from "cookie-parser";
 import path from "path"
 
 const port = process.env.PORT || 5000
@@ -16,9 +16,9 @@ app.use(express.static(path.join(__dirname, './public')))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
-app.use(cookieParser());
+//app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(bodyParser.json());
+//app.use(cookieParser());
 
 // app.use(helmet())
 // app.disable('x-powered-by')
