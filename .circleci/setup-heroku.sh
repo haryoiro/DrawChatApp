5886b174a458cac256fb17304e416d59ff91fdc7
haryoiro <55312590+haryoiro@users.noreply.github.com>
2020-03-13 19:25:07 +0900

    Update setup-heroku.sh

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,3 +1,4 @@
+git remote add heroku https://git.heroku.com/app-drawn.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
@@ -7,9 +8,9 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
-# machine git.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
 
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
